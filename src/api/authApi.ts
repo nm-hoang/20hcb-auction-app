33de082e35f34399f0526b73073542e1d7229a6d
hoangnguyen <nmhoang.28@gmail.com>
2021-10-22 17:35:05 +0700

    Update profile

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -3,7 +3,7 @@ import { getHeaders } from '../helpers/auth';
 import { ChangePasswordType } from '../types/authType';
 
 const prefix = '/auth';
-
+const accountsUrl = '/accounts';
 const authApi = {
   login: (data: object) => {
     const url = `${prefix}/login`;
@@ -19,9 +19,12 @@ const authApi = {
   getMe: () => {
     const url = `${prefix}/me`;
     const headers = getHeaders();
-
     return axiosClient.get(url, headers)
       .then((res) => res);
+  },
+  updateProfile: (profile: object) => {
+    const url = `${accountsUrl}`;
+    return axiosClient.put(url, profile);
   },
 
   // TODO: Fix api, return all with 200 status
--- src/features/profile/pages/EditProfilePage.tsx
+++ src/features/profile/pages/EditProfilePage.tsx
@@ -1,16 +1,118 @@
-import React from 'react';
-import { Card, Col, Row } from 'antd';
+import React, { useEffect } from 'react';
+import {
+  Card,
+  Col,
+  Row,
+  Form,
+  Input,
+  Skeleton,
+  Button,
+} from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {
+  selectGetMe,
+  getMe,
+  updateProfile,
+  selectRequesting,
+} from '../../auth/authSlice';
 
 function EditProfilePage(): JSX.Element {
+  const dispatch = useDispatch();
+  const profile = useSelector(selectGetMe);
+  const requesting = useSelector(selectRequesting);
+  const [form] = Form.useForm();
+  const handleSubmit = (e: any) => {
+    dispatch(updateProfile(e));
+  };
+  useEffect(() => {
+    if (profile) {
+      form.setFieldsValue({
+        username: profile!.username,
+        email: profile!.email,
+        fullName: profile!.user.fullName,
+        address: profile!.user.address,
+        phoneNumber: profile!.user.phoneNumber,
+      });
+    } else {
+      dispatch(getMe());
+    }
+  }, [profile]);
+
   return (
     <>
       <Row
         justify="center"
+        className="mb-5"
       >
-        <Col>
+        <Col sm={20} md={12}>
           <Card
             title="Edit profile"
-          />
+          >
+            {profile ? (
+              <Form
+                form={form}
+                layout="horizontal"
+                name="profileForm"
+                labelCol={{ span: 6 }}
+                onFinish={handleSubmit}
+              >
+                <Form.Item
+                  label="Username"
+                  name="username"
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item
+                  label="Email"
+                  name="email"
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item
+                  label="Full name"
+                  name="fullName"
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item
+                  label="Address"
+                  name="address"
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item
+                  label="Phone number"
+                  name="phoneNumber"
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item>
+                  <Row justify="center" gutter={10}>
+                    <Col>
+                      <Link to="/profile">
+                        <Button
+                          htmlType="button"
+                        >
+                          Cancel
+                        </Button>
+                      </Link>
+                    </Col>
+                    <Col>
+                      <Button
+                        type="primary"
+                        htmlType="submit"
+                        loading={requesting === true}
+                      >
+                        Confirm
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form.Item>
+              </Form>
+            )
+              : <Skeleton active />}
+          </Card>
         </Col>
       </Row>
     </>
